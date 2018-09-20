0fafc542ec85116ea4b44e0b52fce75509fa5583
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-19 18:25:07 -0700

    Sep 19 2018 Not done

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,20 +1,49 @@
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Hangman {
 	public static void main(String[] args) {
+
 		Hangman game = new Hangman();
+
+	}
+
+	ArrayList<String> words = new ArrayList<String>();
+
+	Hangman() {
 		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
 		frame.setVisible(true);
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(new GridLayout(4, 0));
 		panel.setVisible(true);
 		frame.add(panel);
-		JLabel label = new JLabel();
-		label.setName("guessletter");
-		JLabel label2 = new JLabel();
-		label2.setName("empty");
-		JLabel label3 = new JLabel();
-		label3.setName("howmanylivesleft");
+
+		JLabel label = new JLabel("guessletter", SwingConstants.CENTER);
+		panel.add(label);
+		label.setVisible(true);
+
+		JLabel label2 = new JLabel("empty", SwingConstants.CENTER);
+		panel.add(label2);
+		label2.setVisible(true);
+
+		JLabel label3 = new JLabel("howmanyliveleft", SwingConstants.CENTER);
+		panel.add(label3);
+
+		label3.setVisible(true);
+		String answer = JOptionPane.showInputDialog("HangMan!!How many words would you like to have");
+		int  num = Integer.parseInt(answer);
+		for (int i = 0; i < num; i++) {
+		String word=Utilities.readRandomLineFromFile("src/dictionary.txt");
+		words.add(word);
+		
+		}
+		System.out.println(words);
+
 	}
 }
